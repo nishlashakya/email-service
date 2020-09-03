fcc8b4caa249ac6bd93742c261368c5fd3e0a37f
nishlashakya <nishla.shakya@dragonlaw.com.hk>
2020-09-03 22:58:30 +0545

    refactor

--- src/actions/formActions.js
+++ src/actions/formActions.js
@@ -1,13 +1,14 @@
 export const  sendEmail = (fields) => {
-    fetch('https://nishla-email-sender.herokuapp.com/', {
-        method: 'POST',
-        body: JSON.stringify({...fields}),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then((result) => {
-        return result
-    }).catch((e) => {
-        console.error(e)
-    })
+	fetch('https://nishla-email-sender.herokuapp.com/', {
+		method: 'POST',
+		body: JSON.stringify({...fields}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	}).then((result) => {
+		alert('Email sent')
+		return result
+	}).catch((e) => {
+		console.error(e)
+	})
 }
